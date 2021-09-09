43c74fbea7e41cac1d1aa38dca10aceca4c793de
Roz <roz@rjmunhoz.me>
2021-09-08 22:32:52 -0300

    fix(code generation): fix missing fields
    some banks would not accept the code without the additional iformation field, even though it is not
    required by BACEN. this adds that field to all codes with a "nullish" value (***)
    
    BREAKING CHANGE: The `reusable` param is no longer accepted, since I couldn't find a single bank
    that uses it

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,6 @@ export type Params = {
   amount?: string
   name: string
   city: string
-  reusable?: boolean
 }
 
 const CRC16 = { id: '63', size: '04', value: '' }
@@ -20,11 +19,11 @@ const MERCHANT_CATEGORY_CODE: Field = { id: '52', size: '04', value: '0000' }
 const TRANSACTION_CURRENCY: Field = { id: '53', size: '03', value: '986' }
 const COUNTRY_CODE: Field = { id: '58', size: '02', value: 'BR' }
 
-const POINT_OF_INITIATION_METHOD = (canBeReused: boolean): Field => ({
-  id: '01',
-  size: '02',
-  value: canBeReused ? '11' : '12'
-})
+/**
+ * These fields are not mandatory, but some banks require them.
+ */
+const TXID: Field = { id: '05', size: '03', value: '***' }
+const ADDITIONAL_DATA_FIELD_TEMPLATE = { id: '62', size: '07', value: [TXID] }
 
 const MERCHANT_NAME = (name: string): Field => ({
   id: '59',
@@ -68,10 +67,21 @@ function fieldToString(field: Field): string {
   return `${field.id}${size}${value}`
 }
 
-export function pix({ key, amount, name, city, reusable = true }: Params) {
+/**
+ * Generates a PIX code to be used in transferences.
+ * 
+ * **Some banks do not allow special characters in the PIX code.
+ * Make sure you remove such carachters before calling this function**
+ * @param params Transference information
+ * @param params.key Pix key
+ * @param params.amount Amount to be transfered
+ * @param params.name Receiver name
+ * @param params.city Receiver city
+ * @returns 
+ */
+export function pix({ key, amount, name, city}: Params) {
   const fields: Field[] = [
     PAYLOAD_FORMAT_INDICATOR,
-    POINT_OF_INITIATION_METHOD(reusable),
     MERCHANT_ACCOUNT_INFORMATION(key),
     MERCHANT_CATEGORY_CODE,
     TRANSACTION_CURRENCY
@@ -79,7 +89,7 @@ export function pix({ key, amount, name, city, reusable = true }: Params) {
 
   if (amount) fields.push(TRANSACTION_AMOUNT(amount))
 
-  fields.push(...[COUNTRY_CODE, MERCHANT_NAME(name), MERCHANT_CITY(city), CRC16])
+  fields.push(...[COUNTRY_CODE, MERCHANT_NAME(name), MERCHANT_CITY(city), ADDITIONAL_DATA_FIELD_TEMPLATE, CRC16])
 
   const payload = fields.map(fieldToString).join('')
 
--- test/pix.js
+++ test/pix.js
@@ -1,87 +1,66 @@
-const { expect } = require('chai')
-const { pix } = require('../dist')
+const { expect } = require('chai');
+const { pix } = require('../dist');
 
 describe('pix', () => {
-  it('Generates valid PIX codes', () => {
-    expect(
-      pix({
-        key: '26385709906',
-        amount: '1234.00',
-        city: 'Cidade',
-        name: 'FULANO DE TAL',
-        reusable: true
-      })
-    ).to.equal(
-      '00020101021126330014br.gov.bcb.pix01112638570990652040000530398654071234.005802BR5913FULANO DE TAL6006Cidade63041923'
-    )
+  describe('generates valid PIX codes for', () => {
+    it('non specified amount', () => {
+      expect(
+        pix({
+          key: '26385709906',
+          city: 'Cidade',
+          name: 'FULANO DE TAL'
+        })
+      ).to.equal(
+        '00020126330014br.gov.bcb.pix0111263857099065204000053039865802BR5913FULANO DE TAL6006Cidade62070503***6304151C'
+      );
+    });
 
-    expect(
-      pix({
-        key: '26385709906',
-        amount: '1234.00',
-        city: 'Cidade',
-        name: 'FULANO DE TAL',
-        reusable: false
-      })
-    ).to.equal(
-      '00020101021226330014br.gov.bcb.pix01112638570990652040000530398654071234.005802BR5913FULANO DE TAL6006Cidade6304B2C0'
-    )
-  })
+    it('email keys', () => {
+      expect(
+        pix({
+          key: 'fulano.de.tal@gmail.com',
+          city: 'Cidade',
+          name: 'FULANO DE TAL'
+        })
+      ).to.equal(
+        '00020126450014br.gov.bcb.pix0123fulano.de.tal@gmail.com5204000053039865802BR5913FULANO DE TAL6006Cidade62070503***63045E63'
+      );
+    });
 
-  it('is able to generate codes without a specified amount', () => {
-    expect(
-      pix({
-        key: '26385709906',
-        city: 'Cidade',
-        name: 'FULANO DE TAL'
-      })
-    ).to.equal(
-      '00020101021126330014br.gov.bcb.pix0111263857099065204000053039865802BR5913FULANO DE TAL6006Cidade63047AA0'
-    )
-  })
+    it('random keys', () => {
+      expect(
+        pix({
+          key: '4b7b4eb3-d426-48f1-8ecf-998bda62c0a1',
+          city: 'Cidade',
+          name: 'FULANO DE TAL'
+        })
+      ).to.equal(
+        '00020126580014br.gov.bcb.pix01364b7b4eb3-d426-48f1-8ecf-998bda62c0a15204000053039865802BR5913FULANO DE TAL6006Cidade62070503***6304CAC5'
+      );
+    });
 
-  it('generates valid codes for email keys', () => {
-    expect(
-      pix({
-        key: 'fulano.de.tal@gmail.com',
-        city: 'Cidade',
-        name: 'FULANO DE TAL'
-      })
-    ).to.equal(
-      '00020101021126450014br.gov.bcb.pix0123fulano.de.tal@gmail.com5204000053039865802BR5913FULANO DE TAL6006Cidade63044FA8'
-    )
-  })
-  it('generates valid codes for random keys', () => {
-    expect(
-      pix({
-        key: '4b7b4eb3-d426-48f1-8ecf-998bda62c0a1',
-        city: 'Cidade',
-        name: 'FULANO DE TAL'
-      })
-    ).to.equal(
-      '00020101021126580014br.gov.bcb.pix01364b7b4eb3-d426-48f1-8ecf-998bda62c0a15204000053039865802BR5913FULANO DE TAL6006Cidade63042D6E'
-    )
-  })
-  it('generates valid codes for phone keys', () => {
-    expect(
-      pix({
-        key: '+5568375779606',
-        city: 'Cidade',
-        name: 'FULANO DE TAL'
-      })
-    ).to.equal(
-      '00020101021126360014br.gov.bcb.pix0114+55683757796065204000053039865802BR5913FULANO DE TAL6006Cidade63045035'
-    )
-  })
-  it('generates valid codes for cnpj keys', () => {
-    expect(
-      pix({
-        key: '20687705000136',
-        city: 'Cidade',
-        name: 'FULANO DE TAL'
-      })
-    ).to.equal(
-      '00020101021126360014br.gov.bcb.pix0114206877050001365204000053039865802BR5913FULANO DE TAL6006Cidade63043E7B'
-    )
-  })
-})
+    it('phone keys', () => {
+      expect(
+        pix({
+          key: '+5568375779606',
+          city: 'Cidade',
+          name: 'FULANO DE TAL'
+        })
+      ).to.equal(
+        '00020126360014br.gov.bcb.pix0114+55683757796065204000053039865802BR5913FULANO DE TAL6006Cidade62070503***630484D0'
+      );
+    });
+
+    it('cnpj keys', () => {
+      expect(
+        pix({
+          key: '20687705000136',
+          city: 'Cidade',
+          name: 'FULANO DE TAL'
+        })
+      ).to.equal(
+        '00020126360014br.gov.bcb.pix0114206877050001365204000053039865802BR5913FULANO DE TAL6006Cidade62070503***630472EB'
+      );
+    });
+  });
+});
